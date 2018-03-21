c96dfdf59e2cbcef22fe41f5fd7ec546cd8b2a56
khairunnisa07 <knisa9097@gmail.com>
2018-03-21 10:29:04 +0700

    Latihan 1, Halaman Perdana

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+//ini adlah komentar saja 
+//bagaimana bila
+//engkau wahai komentar
+//berpanjang lebar dalam berkatakata
+
+	echo "Khairun Nisa P <br>";
+	echo "Nim H1101161041 <br>";
+
+	echo "pontianak <br>";
+	echo "pontianak<br>";
+	echo "pontianak <br>";
+	echo "pontianak<br>";
+	echo "pontianak<br>";
+
+
+ ?>
